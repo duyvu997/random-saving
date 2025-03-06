b28bbe98f18029e5cfbbcf503cdc3e94519cb8be
duy-vu <ngocduy0997@gmail.com>
2025-03-07 02:16:42 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'tien-duong-gia-v1';
+const urlsToCache = [
+    '/tien-duong-gia/',
+    '/tien-duong-gia/index.html',
+    '/tien-duong-gia/manifest.json',
+    '/tien-duong-gia/icon-192x192.png',
+    '/tien-duong-gia/icon-512x512.png'
+];
+
+// ... rest of service worker code ...
